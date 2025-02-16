ce4f0d70edacba1eb0307f1f79db23f07e322c96
CamiloJoseVilladaGaviria <cajoviga@gmail.com>
2025-02-15 20:29:39 -0500

    MY PERSONAL PROFILE

---
+++ IMG/utils.js
@@ -0,0 +1,6 @@
+setTimeout(function () {
+  document.getElementById("AfterFace").classList.add("show");
+  document.getElementById("BeforeFace").classList.add("hide");
+}, 6000);
+
+alert("WELCOME TO MY SITE!");
--- utils.js
+++
@@ -1,6 +0,0 @@
-setTimeout(function () {
-  document.getElementById("AfterFace").classList.add("show");
-  document.getElementById("BeforeFace").classList.add("hide");
-}, 6000);
-
-alert("WELCOME TO MY SITE!");
